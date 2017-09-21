d8ff0cdccb88613ba6b479384f4097ef59baae71
dimonlexx <31965436+dimonlexx@users.noreply.github.com>
2017-09-21 18:46:22 +0300

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+package com.helloworld;
+
+import java.io.PrintWriter;
+
+public class helloworld {
+        public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out, true);
+        out.println("Hello, world!");
+    }
+}
